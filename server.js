bdf8934bc0d9f859e069c5865c05cdd3c138c8a8
Alken Dester <96910178+Dester-Alken@users.noreply.github.com>
2022-01-19 19:27:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3/";
 const app = express();
 
 let corsOptions = {
-    origin: process.env.DESTER_FRONTEND_URL,
+    origin: `${process.env.DESTER_FRONTEND_URL}`,
     optionsSuccessStatus: 200
 }
 
